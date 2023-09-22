98d2ebf8ec0ef35a67eb46a92c01215c0e504463
Sebas-montilla <sebastianam127@gmail.com>
2023-09-22 13:01:52 -0400

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://gateway.marvel.com',
+    apiUrl: 'https://gateway.marvel.com',
     ts: '1695309978766',
     apiKey: '51f52024302bca85adcbf30e730694ab',
     hash: '1a82ce7a5046b4f1a72d9b2f0793627e'
